fa744fe8b6f5278fb3d543661fd3b5f734ef6c7c
Prathamesh Prabhakar Kumbhar <86184687+Kumbhar7700@users.noreply.github.com>
2022-02-26 10:42:12 +0530

    Add files via upload
    first commit

---
+++ calc.java
@@ -0,0 +1,13 @@
+public class calc
+{
+    public static void main(String s[])
+    {
+        int a,b;
+        a=20;
+        b=45;
+        int c=a+b;
+
+        System.out.println("sum of the two number is:"+c);
+
+    }
+}
